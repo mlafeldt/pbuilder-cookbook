ff531dfcdcdbc762ecc2dff54721869dcb28cdd0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-01-14 15:21:26 +0100

    Add LWRP example to spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,21 @@
 require 'chefspec'
 
 describe 'The recipe pbuilder::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'pbuilder::default' }
+  let (:chef_run) do
+    chef_run = ChefSpec::ChefRunner.new(:step_into => ['pbuilder_chroot'])
+    chef_run.node.set['pbuilder'] = {
+      "chroots" => {
+        "wheezy64" => {
+          "distribution"    => "wheezy",
+          "architecture"    => "amd64",
+          "mirror"          => "ftp://ftp2.de.debian.org/debian/",
+          "debootstrapopts" => ["--variant=buildd"]
+        }
+      }
+    }
+    chef_run.converge 'pbuilder::default'
+    chef_run
+  end
 
   %w(pbuilder debootstrap devscripts).each do |pkg|
     it "should install the package #{pkg}" do
@@ -11,5 +25,15 @@ describe 'The recipe pbuilder::default' do
 
   it 'should create the pbuilder config file' do
     chef_run.should create_file chef_run.node['pbuilder']['config_file']
+  end
+
+  it 'should create the wheezy64 chroot' do
+    basetgz = ::File.join(chef_run.node['pbuilder']['cache_dir'],
+                          "wheezy-amd64-base.tgz")
+    chef_run.should execute_command \
+      "pbuilder create --basetgz #{basetgz} " \
+      "--distribution wheezy --architecture amd64 " \
+      "--mirror ftp://ftp2.de.debian.org/debian/ " \
+      "--debootstrapopts --variant=buildd"
   end
 end
